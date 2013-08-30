0c94332d9bb4b6ac500f4865bc7ddfddba5eff7e
Laurie <github@seldo.com>
2013-08-29 18:25:07 -0700

    This won't work

--- web.js
+++ web.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var pg = require("pg");
+
 var app = express();
 app.use(express.logger());
 
@@ -7,6 +9,15 @@ app.get('/', function(request, response) {
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+pg.connect(process.env.HEROKU_POSTGRESQL_AMBER_URL, function(err, client, done) {
+  client.query('SELECT * FROM your_table', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
 });
